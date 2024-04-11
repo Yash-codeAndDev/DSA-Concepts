cde25e02ed513a10870ddf8fc5c9477c2b2b635a
Yash Khati <160757711+Yash-codeAndDev@users.noreply.github.com>
2024-04-11 15:03:02 +0000

    BST Creation

---
+++ Tree/BST_template.py
@@ -0,0 +1,47 @@
+class Node:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.val = key
+
+class BinaryTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, key):
+        if self.root is None:
+            self.root = Node(key)
+        else:
+            self._insert_recursive(self.root, key)
+
+    def _insert_recursive(self, node, key):
+        if key < node.val:
+            if node.left is None:
+                node.left = Node(key)
+            else:
+                self._insert_recursive(node.left, key)
+        elif key >= node.val:
+            if node.right is None:
+                node.right = Node(key)
+            else:
+                self._insert_recursive(node.right, key)
+
+    def display_inorder(self):
+        self._display_inorder_recursive(self.root)
+
+    def _display_inorder_recursive(self, node):
+        if node is not None:
+            self._display_inorder_recursive(node.left)
+            print(node.val)
+            self._display_inorder_recursive(node.right)
+    
+
+BST_Tree = BinaryTree()
+num_nodes = int(input("Enter the number of nodes in the tree: "))
+print("Enter the nodes:")
+for _ in range(num_nodes):
+    key = int(input())
+    BST_Tree.insert(key)
+
+print("Inorder Traversal of BST:")
+BST_Tree.display_inorder()
