3c06e3df1fec1350f4f570806c8c2b19c0326328
Yash Khati <160757711+Yash-codeAndDev@users.noreply.github.com>
2024-04-26 15:39:12 +0000

    Shortest Path for source to all Nodes Dijkstra

---
+++ Graphs/DijkstrasAlgo.py
@@ -0,0 +1,60 @@
+import heapq
+
+class Dijkstra:
+
+    def __init__(self) -> None:
+        self.nodes = 0
+        self.edges = 0
+        self.adjList = None
+
+        self.createAdjList()
+    
+    def createAdjList(self):
+        self.nodes = int(input("Enter Number of Nodes : "))
+        self.edges = int(input("Enter Number of Edges : "))        
+        self.adjList = [ [] for _ in range( self.nodes ) ]
+
+        print("Enter Edges : ")
+        for i in range(self.edges):
+            u = int(input("From : "))
+            v = int(input("To : "))
+            w = int(input("Weight : "))
+            self.adjList[u].append((v,w))
+            self.adjList[v].append((u,w))
+            
+
+
+    def printAdjList(self):
+        for i in range(self.nodes):
+            print(" {} -> {} ".format(i, self.adjList[i]))            
+
+
+
+    def shortestPath_Dijskstra(self):
+
+        src = int(input("Enter Source : "))
+        
+        pq = []
+
+        # make pq priority queue -> (dist, source)
+        heapq.heappush(pq , (0,src))
+
+        dist = [ float('inf')] * self.nodes
+        dist[src] = 0
+
+        while pq:
+
+            d, u = heapq.heappop(pq)
+
+            for v, weight in self.adjList[u]:
+                
+                if dist[v] > dist[u] + weight:
+                    dist[v] = dist[u] + weight
+                    heapq.heappush(pq, (dist[v] , v))
+        
+        for i in range(self.nodes):
+            print('{} -> {}'.format(i, dist[i]))
+
+if __name__ == '__main__':
+   obj = Dijkstra()
+   obj.shortestPath_Dijskstra()
