35e7d2f68d821a719362e9869abc210c8b20b642
Yash Khati <160757711+Yash-codeAndDev@users.noreply.github.com>
2024-06-22 14:20:11 +0000

    Segment Tree

--- AdvanceConcepts/SegmentTree.py
+++ AdvanceConcepts/SegmentTree.py
@@ -1,1 +1,21 @@
+arr = [1, 0, 3, 2, 4, 2, 1]
+query = [[1, 3], [3, 6], [2, 5]]
 
+# Initialize the segment tree array
+segTree = [0] * (4 * len(arr))
+
+def buildSegmentTree(index, low, high, arr, segTree):
+    if low == high:
+        segTree[index] = arr[low]
+        return 
+
+    mid = (low + high) // 2
+    
+    buildSegmentTree(2 * index + 1, low, mid, arr, segTree)
+    buildSegmentTree(2 * index + 2, mid + 1, high, arr, segTree)
+    
+    segTree[index] = min(segTree[2 * index + 1], segTree[2 * index + 2])
+
+# Build the segment tree
+buildSegmentTree(0, 0, len(arr) - 1, arr, segTree)
+print(segTree)
